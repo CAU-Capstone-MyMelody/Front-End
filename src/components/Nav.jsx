5323fc06364e4159630e8261ccbdfeb674ccda81
변현석 <bhs3066@gmail.com>
2025-05-01 19:37:14 +0900

    fix : 미사용 변수&종속 제거

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { useNavContext } from "../apis/NavContext";
 
--- src/pages/VideoDetailPage.jsx
+++ src/pages/VideoDetailPage.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { getChannelDetail, getVideoDetail } from "../apis/youtubeapi";
-import TopNav from "../components/TopNav";
 import Nav from "../components/Nav";
 import styled from "styled-components";
 import TopNavBack from "../components/TopNavBack";
